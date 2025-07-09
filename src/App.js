e45b9695e7d673ae3561f516cb35de95d50b95de
macuih <macuihlein2025@u.northwestern.edu>
2025-07-09 16:27:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
 import FactoryContract from "./contracts/Factory.json";
-import getWeb3 from "./utils/getWeb3";
+import Web3 from "web3";
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink
+} from "react-router-dom";
 
 import NewFundraiser from './NewFundraiser';
 import Home from './Home';
@@ -20,45 +27,61 @@ const useStyles = makeStyles({
 });
 
 const App = () => {
-  const [state, setState] = useState({ web3: null, accounts: null, contract: null });
+  const [state, setState] = useState({
+    web3: null,
+    accounts: null,
+    contract: null
+  });
 
   useEffect(() => {
     const init = async () => {
       try {
-        const web3 = await getWeb3();
+        const web3 = new Web3(
+          new Web3.providers.HttpProvider("http://ec2-3-129-52-168.us-east-2.compute.amazonaws.com:8545")
+        );
+
         const accounts = await web3.eth.getAccounts();
         const networkId = await web3.eth.net.getId();
         const deployedNetwork = FactoryContract.networks[networkId];
+
+        if (!deployedNetwork) {
+          throw new Error("Contract not deployed to this network.");
+        }
+
         const instance = new web3.eth.Contract(
           FactoryContract.abi,
-          deployedNetwork && deployedNetwork.address,
+          deployedNetwork.address
         );
+
         setState({ web3, accounts, contract: instance });
       } catch (error) {
         alert("Failed to load web3, accounts, or contract. Check console for details.");
         console.error(error);
       }
     };
+
     init();
   }, []);
 
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       <Router>
-        <AppBar position="static" color="default" style={{ margin: 0 }}>
+        <AppBar position="static" color="default">
           <Toolbar>
-            <Typography variant="h6" color="inherit">
+            <Typography variant="h6" color="inherit" style={{ marginRight: 20 }}>
               <NavLink className="nav-link" to="/">Home</NavLink>
             </Typography>
             <NavLink className="nav-link" to="/new/">New</NavLink>
           </Toolbar>
         </AppBar>
 
-        <Route exact path="/" component={Home} />
-        <Route path="/new/" component={NewFundraiser} />
-        <Route path="/receipts" component={Receipts} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new/" element={<NewFundraiser />} />
+          <Route path="/receipts" element={<Receipts />} />
+        </Routes>
       </Router>
     </div>
   );
